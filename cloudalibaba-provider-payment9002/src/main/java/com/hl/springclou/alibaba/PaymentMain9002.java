737922c57a8dc7d1a71e9a08754b5461da61d2c7
hongyidashi <1031749665@qq.com>
2020-03-17 09:16:17 +0800

    验证nacos负载均衡

--- cloudalibaba-provider-payment9002/src/main/java/com/hl/springclou/alibaba/PaymentMain9002.java
+++ cloudalibaba-provider-payment9002/src/main/java/com/hl/springclou/alibaba/PaymentMain9002.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @Auther: panhongtong
+ * @Date: 2020/3/16 18:18
+ * @Description:
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PaymentMain9002 {
