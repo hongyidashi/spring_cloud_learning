e35e30fcab1de7cde9090a417bce6a1c0e904bba
hongyidashi <1031749665@qq.com>
2020-03-18 09:50:39 +0800

    提交sentinel-熔断降级学习代码

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/hl/springcloud/alibaba/OrderNacosMain84.java
@@ -0,0 +1,24 @@
+package com.hl.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+public class OrderNacosMain84 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain84.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
